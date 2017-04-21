d2efaf2d6e4609a6afd60ce9ab9d130b9d3fc5f0
michaeldey <michaeldey@hotmail.com>
2017-04-20 19:53:07 -0600

    Created constructor

--- a06_WordNet/src/a06_WordNet/WordNet.java
+++ a06_WordNet/src/a06_WordNet/WordNet.java
@@ -1,12 +1,20 @@
 package a06_WordNet;
 
 import edu.princeton.cs.algs4.Queue;
+
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
 
 
 public class WordNet {
+	
+		private ST <Integer,String[]>symbolTree = new ST<Integer, String[]>();
+		private Digraph G;
+		private int vertexCount=0;
 
 	   // constructor takes the name of the two input files
 	   public WordNet(String synsets, String hypernyms)
@@ -15,33 +23,55 @@ public class WordNet {
 		   In hypIn = new In(hypernyms);
 		   
 		   String synLine, synIndex, synNoun;
-		   Integer key;
-		   if (synIn.hasNextLine())	//change this to a while statement after testing
+		   
+		   createSynsetST(synIn); //create symbol table from synset file
+
+		   //set up our Digraph
+		   this.G = new Digraph(vertexCount);
+		   
+		   createHypernymDigraph(hypIn);//create Digraph from hypernyms file
+	   }
+	   
+	   private void createSynsetST(In synIn)
+	   {
+		 //create a symbol tree based on Synsets file, splitting the first value as the key, subsequent values in the line as string array
+		   while (synIn.hasNextLine())		//change this to a while statement after testing
 		   {
-			 String[] values = synIn.readLine().split(",");
+			   vertexCount++;
+			   String[] values = synIn.readLine().split(",");
 			 
 			 //load first value of line as an integer, and use that as the index
 			 try
 			 {
-				 key = Integer.parseInt(values[0]);
-				 
-				 for (int i = 1; i<(values.length-1); i++)
-				 {
-					 System.out.println(i);
-				 }
+				 //send to symbol tree the first value in the line as an integer, the rest of the values are sent as a string array
+				 symbolTree.put(Integer.parseInt(values[0]), Arrays.copyOfRange(values, 1, (values.length-1)));
 			 }catch(NumberFormatException e)
 			 {
 				 System.out.println("File is in incorrect format. First value in line must be an integer. " + e.getMessage());
-			 }	 
-			 
-			 
-		   }
+			 }	 			 
+		   } 
 	   }
 	   
-//	   private class WordNetNode(int index, String[] definitions)
-//	   {
-//		   
-//	   }
+	   private void createHypernymDigraph(In hypIn)
+	   {
+		   while (hypIn.hasNextLine())
+		   {
+			   String[] values = hypIn.readLine().split(",");
+			   
+			   try
+			   {
+				   int V = Integer.parseInt(values[0]);
+				   for (int i = 1; i < values.length; i++)
+				   {
+					   int W = Integer.parseInt(values[i]);
+					   this.G.addEdge(V, W);
+				   }
+			   }catch(NumberFormatException e)
+			   {
+				   System.out.println("File is in incorrect format. All values must be integers." + e.getMessage());
+			   }
+		   }
+	   }
 
 	   // returns all WordNet nouns
 	   public Iterable<String> nouns()
